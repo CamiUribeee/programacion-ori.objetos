a94199c2ef2b4096a3c5681648b277cc44c3570f
CamiUribeee <camirzzz26@gmail.com>
2024-02-29 15:30:33 -0500

    pequeña modificacion

--- programacionII/src/main/java/com/claseufpso/programacionii/ProgramacionII.java
+++ programacionII/src/main/java/com/claseufpso/programacionii/ProgramacionII.java
@@ -11,6 +11,39 @@ package com.claseufpso.programacionii;
 public class ProgramacionII {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Character homero = new Character("homero", 39, 'm', "hola" );
+        homero.saySomething();
+
+        Character bart = new Character ()
     }
+    static class Character {
+        String name;
+        int age;
+        char gender; 
+        String phrase;
+
+        public Character(String name, int age, char gender) {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+            
+        }
+
+        public Character(String name, int age, char gender, String phrase) {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+            this.phrase = phrase;
+        }
+        
+    }
+    
+    void saySomething (){
+        System.out.println();
+        
+    }
+    
+    
+    
 }
+
