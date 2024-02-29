e39a073ad44e7032dfd6717c9c03b5aa2c045602
CamiUribeee <camirzzz26@gmail.com>
2024-02-29 14:34:04 -0500

    creacion del proyecto

---
+++ programacionII/src/main/java/com/claseufpso/programacionii/ProgramacionII.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.claseufpso.programacionii;
+
+/**
+ *
+ * @author SCIS2PC30
+ */
+public class ProgramacionII {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
